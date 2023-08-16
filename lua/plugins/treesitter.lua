d7347c234201dad845b654023088821e488d3a25
jimzhan <jim.zhan@me.com>
2023-08-17 00:04:28 +0800

    syntax on via treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,31 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-
   config = function()
+    require('nvim-treesitter.configs').setup({
+      indent = {
+        enable = true,
+        disable = {},
+      },
+      auto_install = true,
+      ensure_installed = {
+        'lua',
+        'java',
+        'javascript',
+        'markdown',
+        'markdown_inline',
+        'regex'
+      },
+      sync_install = false,
+      auto_install = true,
+      ignore_install = {},
 
-  end
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    })
+
+    -- let it to use 'markdown' parser for mdx filetype.
+    vim.treesitter.language.register('markdown', 'mdx')
+  end,
 }
