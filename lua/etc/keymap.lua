c9a99086485bf0f8221a2d42dafcc72d989a4bf2
jimzhan <jim.zhan@me.com>
2024-07-07 11:28:34 +0800

    update to barbar.nvim keymaps

--- lua/etc/keymap.lua
+++ lua/etc/keymap.lua
@@ -1,13 +1,19 @@
 local map = vim.keymap.set
-local defaults = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true }
 -----------------------------
 -- Neovim shortcuts
 -----------------------------
 vim.g.mapleader = ','
 -----------------------------
--- Buffer
+-- Buffer (native)
 -----------------------------
-map('n', '<C-l>', ':bnext<CR>',     defaults)
-map('n', '<C-h>', ':bprevious<CR>', defaults)
-map('n', '<C-k>', ':bdelete<CR>',   defaults)
-
+-- map('n', '<C-l>', ':bnext<CR>',     opts)
+-- map('n', '<C-h>', ':bprevious<CR>', opts)
+-- map('n', '<C-k>', ':bdelete<CR>',   opts)
+-----------------------------
+-- Buffer (for 'romgrk/barbar.nvim')
+-- SEEALSO for default keymaps: https://github.com/romgrk/barbar.nvim
+-----------------------------
+map('n', '<C-h>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<C-l>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<C-k>', '<Cmd>BufferClose<CR>', opts)
